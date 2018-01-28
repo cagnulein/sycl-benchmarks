31fb4dc2a6afd762112ab7eca2b9579bb8375085
Roberto Viola <cagnulein@gmail.com>
2018-01-28 16:46:17 +0000

    test-all-angles improved (but it's not fair for the GPU because a lot of time on the software is CPU based)

--- rotate/test-all-angles.sh
+++ rotate/test-all-angles.sh
@@ -1,6 +1,13 @@
+TIMEFORMAT=%R
+echo -e "Angle\tCPU 1 Core\tGPU"
 for i in {0..359}
 do
         echo
-	echo "Angle: " $i;
-	time ./rotate.gpu $i;
+	exec 3>&1 4>&2
+	CPU=$({ time ./rotate_classic $1 1>&3 2>&4; } 2>&1);
+	exec 3>&- 4>&-
+	exec 3>&1 4>&2
+	GPU=$({ time ./rotate.gpu $i 1>&3 2>&4; } 2>&1);
+	exec 3>&- 4>&-
+	echo -en $i"\t"$CPU"\t\t"$GPU;
 done
