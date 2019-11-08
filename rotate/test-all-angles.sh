465e5c190a4ae9c34f45df5daa914156b2ad8d64
Roberto Viola <cagnulein@gmail.com>
2019-11-08 16:36:29 +0000

    fix typo on test-all-angles

--- rotate/test-all-angles.sh
+++ rotate/test-all-angles.sh
@@ -4,7 +4,7 @@ for i in {0..359}
 do
         echo
 	exec 3>&1 4>&2
-	CPU=$({ time ./rotate_classic $1 1>&3 2>&4; } 2>&1);
+	CPU=$({ time ./rotate_classic $i 1>&3 2>&4; } 2>&1);
 	exec 3>&- 4>&-
 	exec 3>&1 4>&2
 	GPU=$({ time ./rotate.gpu $i 1>&3 2>&4; } 2>&1);
