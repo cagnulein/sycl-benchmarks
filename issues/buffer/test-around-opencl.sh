2ed7a3bc1f3f9a3dd8bc431a9623e933584b251b
Roberto Viola <cagnulein@gmail.com>
2019-11-18 15:21:16 +0000

    test-around added for buffer issue

---
+++ issues/buffer/test-around-opencl.sh
@@ -0,0 +1,16 @@
+TIMEFORMAT=%R
+echo -e "Index\tTime 1st try\tTime 2nd try\tTime 3rd try"
+for i in {20000..22607}
+do
+        echo
+	exec 3>&1 4>&2
+	GPU1=$({ time ./rotate-opencl.gpu $i 40000 1>&3 2>&4; } 2>&1);
+	exec 3>&- 4>&-
+	exec 3>&1 4>&2
+	GPU2=$({ time ./rotate-opencl.gpu $i 40000 1>&3 2>&4; } 2>&1);
+	exec 3>&- 4>&-
+	exec 3>&1 4>&2
+	GPU3=$({ time ./rotate-opencl.gpu $i 40000 1>&3 2>&4; } 2>&1);
+	exec 3>&- 4>&-
+	echo -en $i"\t"$GPU1"\t\t"$GPU2"\t\t"$GPU3;
+done
